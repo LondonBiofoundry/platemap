f772901a23a63b35a2b1526a161450abb9817beb
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-02-20 17:50:25 +0000

    remove site config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "My Site",
   tagline: "The tagline of my site",
-  url: "https://Benedict-Carling.github.io",
+  url: "https://benedict-carling.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
