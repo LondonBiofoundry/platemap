2c35ef9c785061dba7bb1304c58c0377a77353c1
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-02-20 10:13:54 +0000

    adds gitignore

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="platemap",
-    version="0.0.1b",
-    url="",
-    author="Benedict Carling"
-    author_email="benedict.carling18@imperial.ac.uk"
+    version="0.0.2b",
+    url="https://github.com/Benedict-Carling/platemap",
+    author="Benedict Carling",
+    author_email="benedict.carling18@imperial.ac.uk",
     license="MIT",
     description="A package for working with plates",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(
         exclude=("tests","docsource")
-    ),  # package_data not correctly migrated if "where" arg used.
+    ),
     include_package_data=True,
     classifiers=[
         "Intended Audience :: Science/Research",
@@ -26,9 +26,4 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     python_requires=">=3.7",
-    #install_requires=["biopython>=1.78", "python-Levenshtein", "sbol2"],
-    #project_urls={
-    #    "Documentation": "https://londonbiofoundry.github.io/basicsynbio/index.html",
-    #    "Source": "https://github.com/LondonBiofoundry/basicsynbio",
-    #},
 )
