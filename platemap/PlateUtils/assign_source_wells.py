5f8a095a7e0822d465681b6e33da2a3bc7dd0d58
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-02-22 12:20:31 +0000

    add alternate wells to assign_wells

--- platemap/PlateUtils/assign_source_wells.py
+++ platemap/PlateUtils/assign_source_wells.py
@@ -4,7 +4,7 @@ import math
 
 
 def assign_source_wells(
-    plate: Plate, input_dict: dict[str, float], fill_to: int = None
+    plate: Plate, input_dict: dict[str, float], fill_to: int = None, alternate_wells = False
 ):
     useable_liquid_per_well = (
         fill_to - plate.deadspace if fill_to else plate.well_volume - plate.deadspace
@@ -21,11 +21,20 @@ def assign_source_wells(
                 )
             )
         ):
-            plate.set_well_id(plate.wells[well_index], item)
-            add_volume(
-                plate,
-                plate.wells[well_index],
-                fill_to if fill_to else plate.well_volume,
-                item,
-            )
+            if !alternate_wells:
+                plate.set_well_id(plate.wells[well_index], item)
+                add_volume(
+                    plate,
+                    plate.wells[well_index],
+                    fill_to if fill_to else plate.well_volume,
+                    item,
+                )
+            else:
+                plate.set_well_id(plate.wells[well_index*2], item)
+                add_volume(
+                    plate,
+                    plate.wells[well_index*2],
+                    fill_to if fill_to else plate.well_volume,
+                    item,
+                )
             well_index += 1
